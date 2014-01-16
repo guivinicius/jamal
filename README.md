19e19f8535a5b3a612c23ee6e409d7a5cc369317
Guilherme Vinicius Moreira <gui.vinicius@gmail.com>
2014-01-15 22:17:15 -0200

    Initial commit

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
-jamal
-=====
+# jamal
 
 Jamal is a open-source application for managing phone branchs.
+
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Jamal::Application.config.session_store :cookie_store, key: '_jamal_session'
