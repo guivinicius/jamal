e985518ac367fbb3dcd48ad9b92c97a5d8518f26
Guilherme Vinicius Moreira <gui.vinicius@gmail.com>
2014-01-21 20:06:15 -0200

    Removing ruby directive from gemfile and fixing unicorn log in developemnt

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -27,4 +27,7 @@ Jamal::Application.configure do
   # This option may cause significant delays in view rendering with a large
   # number of complex assets.
   config.assets.debug = true
+
+  config.logger = Logger.new(STDOUT)
+  config.logger.level = Logger.const_get('DEBUG')
 end
